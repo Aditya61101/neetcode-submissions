726275ff78dcdd4b63493aaad47aa8afd3eba82b
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-04-13 19:50:55 +0530

    Add: sliding-window-maximum - submission-2

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>ans; // stores max value of each window
+        deque<int>dq; // stores indexes such that the values at those indexes are in decreasing order
+        int n=nums.size();
+        for(int j=0;j<n;j++) {
+            // helps in maintaining a montonic decreasing deque
+            while(dq.size() && nums[dq.back()] < nums[j]) {
+                dq.pop_back();
+            }
+            dq.push_back(j);
+            if(dq.size() && dq.front()==j-k) dq.pop_front();
+            if(j>=k-1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
