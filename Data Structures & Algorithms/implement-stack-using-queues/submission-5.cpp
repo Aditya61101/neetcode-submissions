2601b7a62a86a126991926e06e20b5e5b6dbe34c
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-04-19 21:16:57 +0530

    Add: implement-stack-using-queues - submission-5

---
+++ Data Structures & Algorithms/implement-stack-using-queues/submission-5.cpp
@@ -0,0 +1,39 @@
+// in this case juggling happens during write, so its perfect for read heavy.
+class MyStack {
+    queue<int>q;
+public:
+    MyStack() {}
+    
+    void push(int x) {
+        q.push(x);
+        int sz=q.size();
+        while(sz>1) {
+            q.push(q.front());
+            q.pop();
+            sz--;
+        }
+    }
+    
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
