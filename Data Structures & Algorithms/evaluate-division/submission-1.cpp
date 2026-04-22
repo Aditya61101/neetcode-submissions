d7652e066e3281aeb476c0c70306f0e4bff18c19
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-04-22 09:24:11 +0530

    Add: evaluate-division - submission-1

---
+++ Data Structures & Algorithms/evaluate-division/submission-1.cpp
@@ -0,0 +1,35 @@
+// T.C: O(Q*(V+E)), S.C: O(V+E)
+class Solution {
+public:
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string,vector<pair<string,double>>>adj;
+        for(int i=0;i<equations.size();i++) {
+            adj[equations[i][0]].push_back({equations[i][1],values[i]});
+            adj[equations[i][1]].push_back({equations[i][0], 1/values[i]});
+        }
+        unordered_map<string,unordered_map<string,double>>ratio;
+        for(const auto &[src, _]: adj) {
+            queue<pair<string,double>>q;
+            unordered_set<string>visited;
+            q.push({src,1.0}); visited.insert(src);
+            while(q.size()) {
+                auto [node,val] = q.front();
+                q.pop();
+                ratio[src][node]=val;
+                for(auto [nei,wt]:adj[node]){
+                    if(visited.count(nei)) continue;
+                    visited.insert(nei);
+                    q.push({nei,val*wt});
+                }
+            }
+        }
+        vector<double>ans;
+        for(auto query:queries) {
+            if(!ratio.count(query[0]) || !ratio[query[0]].count(query[1])) {
+                ans.push_back(-1.0); continue;
+            }
+            ans.push_back(ratio[query[0]][query[1]]);
+        }
+        return ans;
+    }
+};
