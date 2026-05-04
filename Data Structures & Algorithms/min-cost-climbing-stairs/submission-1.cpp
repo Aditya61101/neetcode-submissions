7c0fac0a94e3eb57e58f5daf63d3ac30ee6d528d
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-05-04 18:59:00 +0530

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    // dp[i] represents min cost to reach n starting from i
+    // T.C: O(n), S.C: O(n)
+    vector<int>dp;
+    int n;
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        n = cost.size();
+        dp.resize(n+1,-1);
+        dp[n]=0;
+        dp[n-1]=cost[n-1];
+        for(int j=0;j<=1;j++) {
+            for(int i=n-2;i>=j;i--) {
+                dp[i]=cost[i]+min(dp[i+1],dp[i+2]);
+            }
+        }
+        return min(dp[0],dp[1]);
+    }
+};
