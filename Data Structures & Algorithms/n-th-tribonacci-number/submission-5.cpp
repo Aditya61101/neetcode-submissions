2529a635e437ac8e0c65cdd52d1ef3df01579279
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-05-04 20:12:50 +0530

    Add: n-th-tribonacci-number - submission-5

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-5.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    // tabulation: top-bottom, space optimized
+public:
+    int tribonacci(int n) {
+        if(n==0) return 0;
+        int p1=0,p2=1,p3=1;
+        for(int i=3;i<=n;i++) {
+            int curr=p1+p2+p3;
+            p1=p2;
+            p2=p3;
+            p3=curr;
+        }
+        return p3;
+    }
+};
