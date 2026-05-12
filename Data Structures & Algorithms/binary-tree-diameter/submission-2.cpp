90970971d3d2bff211eb56f67b93a814541bf323
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-05-12 08:46:48 +0530

    Add: binary-tree-diameter - submission-2

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-2.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+    unordered_map<TreeNode*,int>computedHeight;
+    int findHeight(TreeNode* root) {
+        if(!root) return 0;
+        if(computedHeight.count(root)) return computedHeight[root];
+        return computedHeight[root]=1+max(findHeight(root->left), findHeight(root->right));
+    }
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if(!root) return 0;
+        int leftHeight = findHeight(root->left);
+        int rightHeight = findHeight(root->right);
+        
+        int currDiameter = leftHeight+rightHeight;
+        int leftSubtreeDiameter = diameterOfBinaryTree(root->left);
+        int rightSubtreeDiameter = diameterOfBinaryTree(root->right);
+        // for current root, its maximum diameter is
+        // diameter through root
+        // leftsubtree diameter
+        // rightsubtree diameter
+        return max({currDiameter, leftSubtreeDiameter, rightSubtreeDiameter});
+    }
+};
