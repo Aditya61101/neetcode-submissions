80c1ccd98fca048bcd27d0558f7ef299e3036490
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-05-28 13:01:12 +0530

    Add: partition-equal-subset-sum - submission-1

---
+++ Data Structures & Algorithms/partition-equal-subset-sum/submission-1.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    //dp[i][requiredSum] -> can we have 'requiredSum' starting from 'i'
+    bool solve(int i, int target, vector<int>&nums, vector<vector<int>>&dp) {
+        if(i==nums.size()) return false;
+        if(target==0) return true;
+        if(dp[i][target]!=-1) return dp[i][target];
+        
+        for(int j=i;j<nums.size();j++){
+            if(target < nums[j]) continue;
+            if(solve(j+1,target-nums[j],nums,dp)) return dp[i][target]=true;
+        }
+        
+        return dp[i][target]=false;
+    }
+public:
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int totalSum = accumulate(nums.begin(),nums.end(),0);
+        // odd totalSum can never be divided into two equal halves
+        if(totalSum%2 != 0) return false;
+
+        int requiredSum = totalSum/2;
+        // we need to find whether there's exist at least one subset 
+        // whose sum is equal to requriedSum
+        vector<vector<int>>dp(n,vector<int>(requiredSum+1,-1));
+        return solve(0,requiredSum,nums,dp);
+    }
+};
