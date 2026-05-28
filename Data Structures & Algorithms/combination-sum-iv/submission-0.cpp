25c3eb3948443dc3ee54243af881f23ca9df5203
Aditya Kumar <75529750+Aditya61101@users.noreply.github.com>
2026-05-28 15:06:55 +0530

    Add: combination-sum-iv - submission-0

---
+++ Data Structures & Algorithms/combination-sum-iv/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int solve(int target, vector<int>& nums, vector<int>&dp) {
+        if(target==0) return 1;
+        if(dp[target]!=-1) return dp[target];
+
+        int count = 0;
+        for(int num:nums) {
+            if(num<=target) count+=solve(target-num,nums,dp);
+        }
+        return dp[target]=count;
+    }
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int>dp(target+1,-1);
+        return solve(target,nums,dp);
+    }
+};
